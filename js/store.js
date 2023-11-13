ee14bd6ec225b7f37c53cd9d01187b35da260b6b
Juanigpunte <141692130+Juanigpunte@users.noreply.github.com>
2023-11-13 09:52:21 -0300

    Update store.js

--- js/store.js
+++ js/store.js
@@ -1,7 +1,7 @@
 /*---------------------------- MIS PRODUCTOS ----------------------------*/
 
 // Productos almacenados en archivo JSON
-const url = "js/productos.json";
+const url = "../../js/productos.json";
 
 fetch(url)
   .then(response => response.json())
